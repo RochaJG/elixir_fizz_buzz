f26cf5e9445a738e0e228fa023dbc3aadbd7edb0
Jordan Rocha <70rd23@gmail.com>
2020-10-21 00:18:03 -0300

    Added function to convert numbers to integer list

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -5,6 +5,11 @@ defmodule FizzBuzz do
     |> handle_file()
   end
 
-  def handle_file({:ok, result}), do: result
-  def handle_file({:error, reason}), do: reason
+  def handle_file({:ok, result}) do
+    result
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def handle_file({:error, reason}), do: "Error reading file! #{reason}"
 end
